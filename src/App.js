726f3468813d61ae29bae15b8fd2b8ec6a120f27
leesol <jbkljo@naver.com>
2022-07-11 11:55:10 +0900

    리스트 폼 만들기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
 
-import './App.css';
-import DiaryEditor from './DiaryEditor';
-
+const dummyList = [
+  {
+    id: 1,
+    author: "작성자1",
+    content: "하이 1",
+    emotion: 5,
+    create_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "작성자2",
+    content: "하이 2",
+    emotion: 4,
+    create_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "작성자3",
+    content: "하이 3",
+    emotion: 3,
+    create_date: new Date().getTime(),
+  },
+  {
+    id: 4,
+    author: "작성자4",
+    content: "하이 4",
+    emotion: 2,
+    create_date: new Date().getTime(),
+  },
+  {
+    id: 5,
+    author: "작성자5",
+    content: "하이 5",
+    emotion: 1,
+    create_date: new Date().getTime(),
+  },
+];
 function App() {
   return (
     <div className="App">
-      <DiaryEditor/>
+      <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
