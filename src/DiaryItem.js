7c142a067ab3890a5fca045e8f63f4869549e835
leesol <jbkljo@naver.com>
2022-07-12 21:33:29 +0900

    최적화 하기

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 const DiaryItem = ({
   onEdit,
   onRemove,
@@ -72,4 +72,4 @@ const DiaryItem = ({
   );
 };
 
-export default DiaryItem;
+export default React.memo(DiaryItem);
